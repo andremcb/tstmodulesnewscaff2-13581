5adf41f6f409266f1baddb0f06c9fe2db7555d19
André Machado <csantos.machado@gmail.com>
2020-12-17 21:00:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tstmodulesnewscaff2_13581/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tstmodulesnewscaff2_13581;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "tstmodulesnewscaff2_13581";
+    }
+}
